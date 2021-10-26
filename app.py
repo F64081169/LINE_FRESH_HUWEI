c812975ffd289da67ff89e8b13f75b0c91def5d3
F64081169 <62001405+F64081169@users.noreply.github.com>
2021-10-26 18:15:38 +0800

    multipages

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def homepage():
 
     return render_template("index.html")
 
+@app.route('/page2/')
+def page2():
+
+    return render_template("page2.html")
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
